2ab9fa010ba7df7e263ef58420e3bdaff07c7566
bottles10 <henryaovare@gmail.com>
2023-04-03 21:17:56 +0000

    Add method and css for line items total in quote show page

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,4 +4,8 @@ class LineItem < ApplicationRecord
   validates :name, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :unit_price, presence: true, numericality: { greater_than: 0 }
+
+  def total_price
+    quantity * unit_price
+  end
 end
