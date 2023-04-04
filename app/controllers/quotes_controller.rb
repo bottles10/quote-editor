f1ed9ee5171b96a0c69ab5063abf225881de3386
bottles10 <henryaovare@gmail.com>
2023-04-04 00:36:17 +0000

    A robucop style refactor

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,63 +1,61 @@
 class QuotesController < ApplicationController
-    before_action :set_quote, only: [:show, :edit, :update, :destroy]
+  before_action :set_quote, only: %i[show edit update destroy]
 
+  def index
+    @quotes = current_company.quotes.ordered
+  end
 
-    def index
-        @quotes = current_company.quotes.ordered
+  def show
+    @line_item_dates = @quote.line_item_dates.includes(:line_items).ordered
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = current_company.quotes.build(quote_params)
+
+    respond_to do |format|
+      if @quote.save
+        format.html {redirect_to quote_url(@quote), notice: 'Quote was successfully created'}
+        format.turbo_stream { flash.now[:notice] = 'Quote was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def show
-        @line_item_dates = @quote.line_item_dates.includes(:line_items).ordered
+  def edit; end
+
+  def update
+    if @quote.update(quote_params)
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully updated" }
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
+      end
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @quote = Quote.new
+  def destroy
+    @quote.destroy
+
+    respond_to do |format|
+      format.html {redirect_to quotes_path, notice: "Quote was successfully destroyed" }
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed" }
     end
+  end
 
-    def create
-        @quote = current_company.quotes.build(quote_params)
-
-            respond_to do |format|
-                if @quote.save
-                    format.html {redirect_to quote_url(@quote), notice: "Quote was successfully created"}
-                    format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
-                else
-                   format.html { render :new, status: :unprocessable_entity }
-                end
-            end
-    end
-
-    def edit
-    end
-
-    def update
-        if @quote.update(quote_params)
-            respond_to do |format|
-                format.html { redirect_to quotes_path, notice: "Quote was successfully updated" }
-                format.turbo_stream { flash.now[:notice] = "Quote was successfully updated." }
-            end
-            else
-                render :edit, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @quote.destroy
-
-        respond_to do |format|
-            format.html {redirect_to quotes_path, notice: "Quote was successfully destroyed" }
-            format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed" }
-        end
-    end
-
-    private
+  private
 
 
-    def set_quote
-        @quote = current_company.quotes.find(params[:id])
-    end
+  def set_quote
+    @quote = current_company.quotes.find(params[:id])
+  end
 
-    def quote_params
-        params.require(:quote).permit(:name)
-    end
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
 end
